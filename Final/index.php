dca1c2e2f60b86242ffd9184b766ab27480d103c
unknown <lobosg1@.active.newpaltz.edu>
2013-09-09 16:20:45 -0400

    hw

--- Final/index.php
+++ Final/index.php
@@ -1,2 +1,28 @@
-Hello is final
-which is going to be an E-comerce site
+
+<html>
+	<head>
+		
+		<title> Bootstrap 101 Templates</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		
+		<link href="netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" media="screen">
+		</head>
+		
+		
+		<body>
+		<h1> this is final</h1>
+		<? 
+		$msg = 'hello';
+		$name = 'Gerson';
+		include 'file';
+		
+		
+		
+		?>
+		
+		<span class= "label label-success"> <?= echo $msg . $name;  ?> </$></span>
+		
+		<script src="/Final/code.jquery.com/jquery.js"></script>
+		<script src="netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+	</body>
+</html>
