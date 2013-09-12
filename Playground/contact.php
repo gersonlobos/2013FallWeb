04520f7694dedda0ebf756a140c1df08dab2b9bf
unknown <lobosg1@.active.newpaltz.edu>
2013-09-12 14:52:08 -0400

    final for hw1

--- Playground/contact.php
+++ Playground/contact.php
@@ -37,6 +37,7 @@
 				        </ul>
 				      </li>
       			</ul>
+
 				<form class="navbar-form pull-right">
               <input class="span2" type="text" placeholder="Email">
               <input class="span2" type="password" placeholder="Password">
@@ -45,28 +46,43 @@
 			</div>
 		</div>
 	</div>
-	
-	
 		  	<div class="container">
   		<div class="well">
 		    <h1>Hello world!</h1>
-		    <a href="https://www.facebook.com/gerson.lobos" class="btn btn-lg btn-success"> Gerson Lobos</a>
-		    <p> Web Programing</p>
+		    <p>Welcome class of 2013 to Web Server Programming <a class="btn btn-lg btn-success">Learn More</a></p>
   			
   		</div>
-  		
-  		
-  		
-  		
-  		
-      	</div>
-      
+  		<div class="row">
+  			<div class="col-lg-8 col-lg-offset-2"> 
+		  		<h2>Contact Us</h2>
+		  		<form class="form-horizontal">
+				  <div class="form-group">
+				    <label for="inputEmail1" class="col-md-2 control-label">Your Email</label>
+				    <div class="col-md-10">
+				      <input type="email" class="form-control" id="inputEmail1" placeholder="Email">
+				    </div>
+				  </div>
+				  <div class="form-group">
+				    <label for="txtMsg" class="col-md-2 control-label">Message</label>
+				    <div class="col-md-10">
+				      <textarea class="form-control" id="txtMsg" placeholder="Write message here"></textarea>
+				    </div>
+				  </div>
+				  <div class="form-group">
+				    <div class="col-md-offset-2 col-md-10">
+				      <input type="submit" class="btn btn-lg btn-success" value="Submit" />
+				    </div>
+				  </div>  			
+		  		</form>
+  			</div>
+  		</div>
+  		      
    	</div>
     <script src="http://code.jquery.com/jquery.js"></script>
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
     <script type="text/javascript"> 
     	$(function(){
-    		$(".links").addClass("active");
+    		$(".contact").addClass("active");
     	})
     </script>
   </body>
